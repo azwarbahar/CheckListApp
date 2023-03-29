35fb343be7d083c231038f711d2c4d4eaf880b67
azwarbahar07 <azwarbahar07@gmail.com>
2023-03-29 19:11:49 +0800

    fix register

--- app/src/main/java/com/azwar/checklistapp/RegisterActivity.kt
+++ app/src/main/java/com/azwar/checklistapp/RegisterActivity.kt
@@ -2,10 +2,70 @@ package com.azwar.checklistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Toast
+import com.azwar.checklistapp.data.Responses
+import com.azwar.checklistapp.databinding.ActivityRegisterBinding
+import com.azwar.checklistapp.network.ApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var apiClient: ApiClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        apiClient = ApiClient()
+
+        binding.tvLogin.setOnClickListener { finish() }
+
+        binding.btnRegister.setOnClickListener {
+            var email = binding.etEmail.text
+            var username = binding.etUsername.text
+            var password = binding.etPassword.text
+            startRegister(email, username, password)
+        }
+
+    }
+
+    private fun startRegister(email: Editable?, username: Editable?, password: Editable?) {
+
+        apiClient.getApiService()
+            .register(
+                "Bearer " + Constants.TOKEN,
+                email.toString(),
+                password.toString(),
+                username.toString()
+            )
+            .enqueue(object : Callback<Responses.AuthResponse> {
+                override fun onFailure(
+                    call: Call<Responses.AuthResponse>,
+                    t: Throwable
+                ) {
+                    Toast.makeText(this@RegisterActivity, t.message, Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onResponse(
+                    call: Call<Responses.AuthResponse>,
+                    response: Response<Responses.AuthResponse>
+                ) {
+                    val pesanRespon = response.message()
+                    val message = response.body()?.message
+                    val kode = response.body()?.statusCode
+                    Toast.makeText(this@RegisterActivity, message, Toast.LENGTH_SHORT).show()
+
+                    binding.etEmail.setText("")
+                    binding.etUsername.setText("")
+                    binding.etPassword.setText("")
+                }
+            })
+
+
     }
 }
